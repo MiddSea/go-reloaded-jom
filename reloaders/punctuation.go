1a9e35d39f494a3bf86f01060d03a3f020f06146
smiddleto <sean@middleton.fi>
2024-12-30 17:08:10 +0200

    Waiting to commit all of this

--- reloaders/punctuation.go
+++ reloaders/punctuation.go
@@ -5,22 +5,22 @@ import (
 )
 
 // PunctuationFormat formats the punctuation marks in a string
-func PunctuationFormat(line string) string {
+func PunctuationShift(line string) string {
 	// Regular expression to match any spaces around single punctuation marks
-	spaces := regexp.MustCompile(`\s*([.,!?:;])`)
+	spacesBfPunc := regexp.MustCompile(`\s*([.,!?:;])`)
 	// Regular expression to match any spaces between single quotes
 	spBtwnSglQuotes := regexp.MustCompile(`'\s*(.*?)\s*'`)
 	// Regular expression to match any spaces around single punctuation marks before an alphabetical character
-	spSglPunc := regexp.MustCompile(`([.,!?:;])([[:alpha:]])`)
+	puncAlpha := regexp.MustCompile(`([.,!?:;])([[:alpha:]])`)
 
 	// Replace the spaces with one space after the punctuation mark
-	line = spaces.ReplaceAllString(line, "$1")
+	line = spacesBfPunc.ReplaceAllString(line, "$1")
 
 	// Replace the spaces between single quotes with no spaces
 	line = spBtwnSglQuotes.ReplaceAllString(line, "'$1'")
 
 	// Replace the spaces with one space after the punctuation mark before an alphabetical character
-	line = re3.ReplaceAllString(line, "$1 $2")
+	line = puncAlpha.ReplaceAllString(line, "$1 $2")
 
 	return line
 }
